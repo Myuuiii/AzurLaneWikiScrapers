8dc309b5d4a73fadb3dee08f7f17f7148b433ddd
Myuuiii <myu@subsilence.nl>
2021-10-11 15:53:04 +0200

    Updated Console project

--- src/AzurLaneWikiScrapersConsole/Program.cs
+++ src/AzurLaneWikiScrapersConsole/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using AzurLaneWikiScrapers;
+using AzurLaneWikiScrapers.Models;
+using Newtonsoft.Json;
+using Spectre.Console;
 
 namespace AzurLaneWikiScrapersConsole
 {
@@ -7,9 +12,20 @@ namespace AzurLaneWikiScrapersConsole
 	{
 		static void Main(string[] args)
 		{
+			AnsiConsole.Write(new Rule("Azur Lane Wiki Scraper"));
 			AzurLaneWikiScraper scrapers = new AzurLaneWikiScraper();
 
+			#region Export Ship Urls
 			string[] urls = scrapers.UrlScraper.Execute();
+			File.WriteAllText("shipUrls.json", JsonConvert.SerializeObject(urls, Formatting.Indented));
+			#endregion
+
+			#region Export Ships
+			List<AzurLaneShip> ships = new List<AzurLaneShip>();
+			foreach (string shipUrl in urls)
+				ships.Add(scrapers.ShipsScraper.Execute(shipUrl));
+			File.WriteAllText("ships.json", JsonConvert.SerializeObject(ships, Formatting.Indented));
+			#endregion
 		}
 	}
 }
