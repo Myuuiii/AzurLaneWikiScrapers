2930a8566864a2baa32b294d704f3788d0e629bb
Myuuiii <mute@mutedevs.nl>
2021-11-15 14:16:37 +0100

    Removed ID from Equipment Source

--- src/AzurLaneWikiScrapers/Models/AzurLaneEquipmentSource.cs
+++ src/AzurLaneWikiScrapers/Models/AzurLaneEquipmentSource.cs
@@ -5,7 +5,6 @@ namespace AzurLaneWikiScrapers.Models
 	public class AzurLaneEquipmentSource
 	{
 		public AzurLaneEquipmentSource() { }
-		public string EquipmentId { get; set; }
 		public string Name { get; set; }
 		public string Url { get; set; }
 		public AzurLaneEquipmentSourceType Type { get; set; }
